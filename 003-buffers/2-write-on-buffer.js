ad3e5cf7db485ed751f507367a4a93efdc113d8b
Continuous Learner <mail@mail.com>
2023-07-07 14:20:59 +0300

    Properties and basic operations on buffer

--- 003-buffers/2-write-on-buffer.js
+++ 003-buffers/2-write-on-buffer.js
@@ -3,3 +3,6 @@ const buff = Buffer.alloc(8);
 buff.write("s", "utf-8");
 
 console.log(buff); // <Buffer 73 00 00 00 00 00 00 00>
+console.log(buff.toJSON()); // JSON representation of buffer
+console.log(buff.length); // get buffer length
+console.log(buff[0]); // access the first element
