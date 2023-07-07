160950d588c4bc6095d3406571797c711b0d37fa
Continuous Learner <mail@mail.com>
2023-07-07 18:45:24 +0300

    [fs project] Open and read the file

--- 004-file-system/5-fs-micro-project.js
+++ 004-file-system/5-fs-micro-project.js
@@ -1,13 +1,15 @@
 const fs = require('fs/promises');
 
 (async () => {
-	const commandFileHadler = await fs.open('./command', 'r');
+	const commandFileHadler = await fs.open('./command.txt', 'r');
 	const watcher = fs.watch('./command.txt');
 
 	// async iterator
 	for await (const event of watcher) {
 		if (event.eventType === 'change') {
 			console.log('The file was changed');
+			const content = await commandFileHadler.read();
+			console.log(content);
 		}
 	}
 })();
