7f9e0ef62b338d6789cb1e317a09c7ade7e45fcd
Continuous Learner <mail@mail.com>
2023-07-08 12:22:57 +0300

    [fs project] Refactor the code in a way to use FileHandler as an EventEmitter

--- 004-file-system/5-fs-micro-project.js
+++ 004-file-system/5-fs-micro-project.js
@@ -4,35 +4,31 @@ const fs = require('fs/promises');
 	const commandFileHadler = await fs.open('./command.txt', 'r');
 	const watcher = fs.watch('./command.txt');
 
-	commandFileHadler.on("change", () => {
-		console.log('event handler is really an event emitter');
+	commandFileHadler.on("change", async () => {
+		// get the size of the file
+		// needed for buffer size allocation
+		const fileSize = (await commandFileHadler.stat()).size;
+
+		// allocate hte buffer with the size of the file
+		const buffer = Buffer.alloc(fileSize),
+			// the location at which we want to start filling the buffer
+			offset = 0,
+			// how many bytes we want to read
+			length = buffer.byteLength,
+			// the position that we want to start reading the file from
+			position = 0;
+
+		// we always want to read the whole content from the biginning all the way to the end
+		const content = await commandFileHadler.read(buffer, offset, length, position);
+
+		console.log('buffer size', buffer);
+		console.log(content);
 	});
 
 	// async iterator
 	for await (const event of watcher) {
 		if (event.eventType === 'change') {
-			console.log('The file was changed');
-
 			commandFileHadler.emit(event.eventType);
-
-			// get the size of the file
-			// needed for buffer size allocation
-			const fileSize = (await commandFileHadler.stat()).size;
-
-			// allocate hte buffer with the size of the file
-			const buffer = Buffer.alloc(fileSize),
-				// the location at which we want to start filling the buffer
-				offset = 0,
-				// how many bytes we want to read
-				length = buffer.byteLength,
-				// the position that we want to start reading the file from
-				position = 0;
-
-			// we always want to read the whole content from the biginning all the way to the end
-			const content = await commandFileHadler.read(buffer, offset, length, position);
-
-			console.log('buffer size', buffer);
-			console.log(content);
 		}
 	}
 })();
