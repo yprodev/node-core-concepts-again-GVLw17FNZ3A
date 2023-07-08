c47e623378f2142b6399d693edcf9ef16faa5d59
Continuous Learner <mail@mail.com>
2023-07-08 12:05:48 +0300

    [fs project] Allocate fixed size for the buffer, depending on the file size. Specify options for the file read, like: offset, length, position.

--- 004-file-system/5-fs-micro-project.js
+++ 004-file-system/5-fs-micro-project.js
@@ -8,7 +8,24 @@ const fs = require('fs/promises');
 	for await (const event of watcher) {
 		if (event.eventType === 'change') {
 			console.log('The file was changed');
-			const content = await commandFileHadler.read();
+
+			// get the size of the file
+			// needed for buffer size allocation
+			const fileSize = (await commandFileHadler.stat()).size;
+
+			// allocate hte buffer with the size of the file
+			const buffer = Buffer.alloc(fileSize),
+				// the location at which we want to start filling the buffer
+				offset = 0,
+				// how many bytes we want to read
+				length = buffer.byteLength,
+				// the position that we want to start reading the file from
+				position = 0;
+
+			// we always want to read the whole content from the biginning all the way to the end
+			const content = await commandFileHadler.read(buffer, offset, length, position);
+
+			console.log('buffer size', buffer);
 			console.log(content);
 		}
 	}
