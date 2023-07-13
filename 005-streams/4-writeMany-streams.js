6fab714f92cbc0ac02ab6a004c977879e5ba8eb7
Continuous Learner <mail@mail.com>
2023-07-13 09:02:14 +0300

    Implement write many logic with the streams

---
+++ 005-streams/4-writeMany-streams.js
@@ -0,0 +1,19 @@
+const fs = require('node:fs/promises');
+
+// Number of iterations: 1000000
+// Execution time: 436 ms
+// CPU: could not event see it :)
+// Memory: could not event see it :) 
+(async () => {
+  console.time('writeMany');
+  const fileHandle = await fs.open('text.txt', 'w');
+  const stream = fileHandle.createWriteStream();
+
+  for (let i = 0; i < 1000000; i++) {
+    const buff = Buffer.from(` ${i} `, 'utf-8');
+    stream.write(buff);
+  }
+
+  console.timeEnd('writeMany');
+})();
+
