bf7cfa923f35e1e8e241d35192129b9ce001d14a
Continuous Learner <mail@mail.com>
2023-07-17 13:56:22 +0300

    This will NOT work, because we haven't implemented our _final method for the custom stream

---
+++ 005-streams/21-custom-writable-write-to-highWaterMark.js
@@ -0,0 +1,64 @@
+const { Writable } = require('node:stream');
+const fs = require('node:fs');
+
+class FileWriteSteam extends Writable {
+  constructor({ highWaterMark, fileName }) {
+    super({ highWaterMark });
+
+    this.fileName = fileName;
+    this.fileDescriptor = null;
+    this.chunks = [];
+    this.chunksSize = 0;
+    this.writesCount = 0;
+  }
+
+  _construct(callback) {
+    fs.open(this.fileName, 'w', (error, fileDescriptor) => {
+      if (error) {
+        callback(error);
+      } else {
+        this.fileDescriptor = fileDescriptor;
+        callback();
+      }
+    });
+  }
+
+  _write(chunk, encoding, callback) {
+    this.chunks.push(chunk);
+    this.chunksSize += chunk.length;
+
+    if (this.chunksSize > this.writableHighWaterMark) {
+      fs.write(this.fileDescriptor, Buffer.concat(this.chunks, (error) => {
+        if (error) {
+          return callback(error);
+        }
+
+        this.chunks = [];
+        this.chunksSize = 0;
+        ++this.writesCount;
+        callback();
+      }));
+    } else {
+      callback();
+    }
+  }
+
+  // _final() {
+  //
+  // }
+  //
+  // _destroy() {
+  //
+  // }
+}
+
+const stream = new FileWriteSteam({ highWaterMark: 1800, fileName: 'src.txt' });
+
+stream.write(Buffer.from('this is the test string'));
+// stream.end(Buffer.from('Our last write'));
+//
+// steam.on('drain', () => {
+//
+// });
+
+
