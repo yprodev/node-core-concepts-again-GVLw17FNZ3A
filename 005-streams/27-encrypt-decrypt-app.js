f2afa58b9db62a0f36b270eabf84d32ac4c5c87f
Continuous Learner <mail@mail.com>
2023-07-18 12:41:50 +0300

    Encrypt the string from source

--- 005-streams/27-encrypt-decrypt-app.js
+++ 005-streams/27-encrypt-decrypt-app.js
@@ -4,6 +4,13 @@ const fs = require('node:fs/promises');
 
 class Encrypt extends Transform {
   _transform(chunk, encoding, callback) {
+
+    for (let i = 0; i < chunk.length; i++) {
+      if(chunk[i] !== 255) {
+        chunk[i] = chunk[i] + 1
+      }
+    }
+
     this.push(chunk);
     callback();
   }
