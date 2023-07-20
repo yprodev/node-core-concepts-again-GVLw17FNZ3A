f19732801521c0f6b01d175c50769150ab180134
Continuous Learner <mail@mail.com>
2023-07-20 12:06:41 +0300

    Create simple http server

---
+++ 006-networking/1-simple-http-server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const PORT = 4090;
+
+const server = http.createServer((req, res) => {
+  const data = { message: 'Hi there!' };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Connection', 'close');
+  res.statusCode = 200;
+  res.end(JSON.stringify(data));
+});
+
+// If you will not specify IP address, the loopback
+// interface will be used by default
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
