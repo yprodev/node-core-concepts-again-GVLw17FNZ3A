3ba803f23b3929623adc34edcd4dee1e7171bd1e
Continuous Learner <mail@mail.com>
2023-07-31 11:29:35 +0300

    Display the simple progress of the uploaded file

--- 006-networking/uploader/1-client.js
+++ 006-networking/uploader/1-client.js
@@ -5,6 +5,23 @@ const PORT = 5050;
 const HOST = '::1';
 const FILEPATH = './text.txt';
 
+function displayProgress(fileSize, data) {
+  // Show the upload progress
+  let uploadedPercentage = 0;
+  let bytesUploaded = 0;
+
+  bytesUploaded += data.length; // add the number of bytes
+  let newUploadedPercentage = Math.floor((bytesUploaded / fileSize) * 100);
+
+  if (
+    newUploadedPercentage % 5 === 0
+    && newUploadedPercentage !== uploadedPercentage
+  ) {
+    uploadedPercentage = newUploadedPercentage; 
+    console.log(`Uploading... ${uploadedPercentage}%`);
+  }
+}
+
 const socket = net.createConnection(
   {
     host: HOST,
@@ -13,12 +30,16 @@ const socket = net.createConnection(
     const filePath = FILEPATH;
     const fileHandle = await fs.open(filePath, 'r');
     const fileReadStream = fileHandle.createReadStream();
+    const fileSize = (await fileHandle.stat()).size;
+
 
     // Reading from source file
     fileReadStream.on('data', (data) => {
       if (!socket.write(data)) {
         fileReadStream.pause();
       }
+
+      displayProgress(fileSize, data);
     });
 
     socket.on('drain', () => {
